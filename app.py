8f4591d29b96d732c4d1f60cf2d2160a78da3a29
Bhupinder Shergill <shergillbs@gmail.com>
2023-08-11 08:29:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def receive_data():
 
     return jsonify({'message': 'Data saved successfully!'}), 200
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
